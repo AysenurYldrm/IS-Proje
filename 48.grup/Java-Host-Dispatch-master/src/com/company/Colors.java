4a0199c4fde339d3d0cbad7734367166ab4810c8
parmenides <104100906+AysenurYldrm@users.noreply.github.com>
2024-01-01 00:19:21 +0300

    Update Colors.java

--- 48.grup/Java-Host-Dispatch-master/src/com/company/Colors.java
+++ 48.grup/Java-Host-Dispatch-master/src/com/company/Colors.java
@@ -4,6 +4,10 @@ package com.company;
 import java.util.ArrayList;
 
 public class Colors {
+    /**
+     * Bu metod, tüm olası RGB renk kombinasyonlarını oluşturur ve bunları bir liste olarak döndürür.
+     * Her bir renk (kırmızı, yeşil, mavi) için, 0'dan 255'e kadar olan değerler arasında
+     * 51'lik artışlarla ilerler. Bu, standart RGB renklerinin basitleştirilmiş bir versiyonunu üretir. */
     public ArrayList<String> getAllColors() {
         ArrayList<String> colors = new ArrayList<>();
         for (int red = 0; red <= 255; red += 51) {
